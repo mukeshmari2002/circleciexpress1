d2a6abecbe05ee4ebffe497632003fff74cc17c5
mukeshmari2002 <mukeshmari2002@gmail.com>
2024-03-28 11:25:15 +0530

    done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use("/example", exampleRouter);
 
 app.get('/', function (req, res) {
-    res.send('Hello Worlds!II');
+    res.send('Hello welcome to all');
 });
 app.listen(8080);
 console.log("Running on port 8080");
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe("Example Test 1", () => {
     const mockResponse = httpMocks.createResponse();
     exampleRouteHandler(mockRequest, mockResponse);
     const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Goodbye drawing!!!!";
+    const expectedResponseBody = "hiii welcome to all!!!!";
     assert(actualResponseBody, expectedResponseBody);
   });
 });
